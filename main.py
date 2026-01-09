0f1c1096378dcd49aa0d14953b0e9aec719544e5
Kartikey <kartikey123del@gmail.com>
2026-01-09 14:50:43 +0530

    Initial commit: Add coin flip website

---
+++ main.py
@@ -0,0 +1,44 @@
+#name = "kartikey"
+
+#first_name = name[:3]
+#last_name = name[5:]
+#full_name = name[::2]
+#reverse_name = name[::-1]
+#print(reverse_name)
+
+#website = "http://google.com"
+#website_2 = "http://youtube.com"
+
+#slice = slice(7,-4)
+
+#print(website_2[slice])
+
+
+#age= int(input("What's your age Nigga..?"))
+
+#if age == 100:
+#    print("DADU PRANAM!")
+#elif age >= 18:
+#    print("pencho bade hogaye")
+#elif age < 0:
+#   print("bosdike paida toh hoja!")
+#else:
+#    print("BENZEEEMAAA....!")
+
+#temp = int (input("what is the temperature outside?: "))
+
+#if not (temp >= 0 and temp <= 30):
+#  print("the temperature is bad today! ")
+#  print("stay inside!")
+  
+#elif not (temp < 0 or temp > 30):
+#  print("the temperature is good today !")
+#  print("go outside!")
+
+name = ""
+
+while {len(name) == 0:
+    name = input("enter youe name: ")
+    }
+
+    print ("Hello "+ name)
